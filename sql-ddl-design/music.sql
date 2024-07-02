2239a37afa8ff8a00c668def502fedb4b9248918
Ludex-dev <luismaguayoiii@gmail.com>
2024-07-01 20:27:44 -0400

    Secondd attempt

--- sql-ddl-design/music.sql
+++ sql-ddl-design/music.sql
@@ -86,18 +86,18 @@ VALUES
 
 
 
-  INSERT INTO songs (title, duration_in_seconds, release_date, album_id, producer_id)
-  VALUES
-    ('MMMBop', 238, '04-15-1997', 1, 1),
-    ('Bohemian Rhapsody', 355, '10-31-1975', 2, 2),
-    ('One Sweet Day', 282, '11-14-1995', 3, 3),
-    ('Shallow', 216, '09-27-2018', 4, 4),
-    ('How You Remind Me', 223, '08-21-2001', 5, 5),
-    ('New York State of Mind', 276, '10-20-2009', 6, 6),
-    ('Dark Horse', 215, '12-17-2013', 7, 7),
-    ('Moves Like Jagger', 201, '06-21-2011', 8, 8),
-    ('Complicated', 244, '05-14-2002', 9, 9),
-    ('Say My Name', 240, '11-07-1999', 10, 10);
+INSERT INTO songs (title, duration_in_seconds, release_date, album_id, producer_id)
+VALUES
+  ('MMMBop', 238, '04-15-1997', 1, 1),
+  ('Bohemian Rhapsody', 355, '10-31-1975', 2, 2),
+  ('One Sweet Day', 282, '11-14-1995', 3, 3),
+  ('Shallow', 216, '09-27-2018', 4, 4),
+  ('How You Remind Me', 223, '08-21-2001', 5, 5),
+  ('New York State of Mind', 276, '10-20-2009', 6, 6),
+  ('Dark Horse', 215, '12-17-2013', 7, 7),
+  ('Moves Like Jagger', 201, '06-21-2011', 8, 8),
+  ('Complicated', 244, '05-14-2002', 9, 9),
+  ('Say My Name', 240, '11-07-1999', 10, 10);
 
 
 INSERT INTO song_artists (song_id, artist_id)
