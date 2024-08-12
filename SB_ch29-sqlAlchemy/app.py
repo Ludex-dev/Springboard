62425f0558c2383eeb138d2c3a0b1a4c96ce3eac
Ludex-dev <59983743+Ludex-dev@users.noreply.github.com>
2024-08-12 02:36:36 -0400

    Update app.py
    Updated app.py for first entry

--- SB_ch29-sqlAlchemy/app.py
+++ SB_ch29-sqlAlchemy/app.py
@@ -4,14 +4,23 @@ from flask import Flask, request, redirect, render_template
 from models import db, connect_db, User
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+with app.app_context():
+    db.create_all()
 
 from flask_debugtoolbar import DebugToolbarExtension
+
+
+app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = "SECRET!"
+app.config['DEBUG_TB_ENABLED'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
 debug = DebugToolbarExtension(app)
 
 @app.route('/')
@@ -23,36 +32,62 @@ def redirect_to_list():
 def list_users():
    """List users and show add form."""
    users = User.query.all()
+
    return render_template("users.html", users=users)
 
-# @app.route("/users/<int:user_id>")
-# def user_detail(user_id):
-#     user = User.query.get_or_404(user_id)
-#     return render_template("user_detail.html, user=user")
+@app.route('/users/new')
+def NewUser_form():
 
-# @app.route('/users/new')
-# def new_user_form():
-#       """Present a form for new user."""
-#       return render_template("new_user.html")
+   return render_template("newUser_form.html")
 
-# @app.route('/users/new', methods=['GET', 'POST'])
-# def create_user():
-#    """Create a new user."""
+@app.route('/users/new', methods=['POST'])
+def NewUser_Add():
+   """Create a new user."""
+
+   fName = request.form['first_name']
+   lName = request.form['last_name']
+   image = request.form.get('profile_pic', 'default_pic.jpg')
    
-#    if request.method == "POST":
-#       # action = request.form.get("action")
-      
-#       # if action == "edit":
-#       #    db.session.commit()
-#       #    return redirect('/users')
+   new_user = User(first_name=fName, last_name=lName, image_url=image)
+   db.session.add(new_user)
+   db.session.commit()
+   return redirect('/users')
 
-#       fName = request.form.get('first_name')
-#       lName = request.form.get('last_name')
-#       image = request.form.get('profile_pic')
-      
-#       new_user = User(first_name=fName, last_name=lName, image_url=image)
-#       db.session.add(new_user)
-#       db.session.commit()
-#       return redirect('/users')
+@app.route("/users/<int:user_id>")
+def User_profile(user_id):
+   """Show Users info."""
+   user = User.query.get_or_404(user_id)
+
+   return render_template("detail.html", user=user)
+
+@app.route("/users/<int:user_id>/edit")
+def User_profileEdit(user_id):
+   """ Edit User Profile. """
+   user = User.query.get_or_404(user_id)
+
+   return render_template("profileEdit.html", user=user)
+
+@app.route("/users/<int:user_id>/edit", methods=['POST'])
+def ProfileEdit_commit(user_id):
+   """Update user."""
+   user = User.query.get(user_id)
+
+   if user:
+      user.first_name = request.form['first_name']
+      user.last_name = request.form['last_name']
+      user.image_url = request.form.get('profile_pic', 'default_pic.jpg')
+      db.session.commit()
+   else:
+      return "User not found", 404
    
-#    render_template("new_user.html")
+   return redirect('/users')
+
+@app.route("/users/<int:user_id>/delete")
+def User_delete(user_id):
+   """Delete User."""
+    
+   user = User.query.get_or_404(user_id)
+   db.session.delete(user)
+   db.session.commit()
+
+   return redirect('/users')
